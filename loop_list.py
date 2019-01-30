9aa5f2d1ea7ab4b1e91696fb5130ce830308992a
Igor Golyanov <thornograph@gmail.com>
2019-01-30 19:28:41 +0300

    add loop list thingy

---
+++ loop_list.py
@@ -0,0 +1,11 @@
+
+def loop_list(the_list: list):
+    """ Return a generator that will infinitely iterate the given list. """
+    list_size = len(the_list)
+    num = 0
+    while num < list_size:
+        yield the_list[num]
+        num += 1
+        if num >= list_size:
+            num = 0
+
