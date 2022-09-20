127530ab67c152b5e2504b26550358affef3ac1e
Igor Golyanov <thornograph@gmail.com>
2022-09-20 22:02:52 +0300

    add usual dec example

--- sorgens_kammer/decorator_args.py
+++ sorgens_kammer/decorator_args.py
@@ -1,8 +1,9 @@
-""" Decorator that accepts an argument and prints arguments of the decorated function """
+import time
 
 
 # Outermost layer 1 - for dec to be able to accept arguments
 def dec_with_arg(log_level):
+    """ Decorator that accepts an argument and prints arguments of the decorated function """
     # Layer 2 - for dec to take a function as an argument
     def actual_dec(func):
         # Innermost layer 3 - the thing doing all the job
@@ -19,6 +20,24 @@ def dec_with_arg(log_level):
 def func_with_any_args(*args, **kwargs):
     pass
 
+
+def usual_dec(func):
+    """ Decorator measuring func execution time. """
+    def wrapped():
+        start = time.time()
+        func()
+        finish = time.time() - start
+        print(f"Function {func.__name__} took {finish:.3f} seconds")
+
+    return wrapped
+
+
+@usual_dec
+def some_func(sleep_time=1):
+    time.sleep(sleep_time)
+
+
+some_func()
 
 # This will print: DEBUG (1, 12) {'k': 'asdasd', 'b': 3}
 func_with_any_args(1, 12, b=3, k='asdasd')
